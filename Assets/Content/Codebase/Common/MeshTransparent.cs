3d142f6807d6e92baee1eefe3a7ef611005f142e
JackLite <jackkdlite@gmail.com>
2022-11-19 17:23:34 +0600

    Transparency some fixes

--- Assets/Content/Codebase/Common/MeshTransparent.cs
+++ Assets/Content/Codebase/Common/MeshTransparent.cs
@@ -5,22 +5,24 @@ namespace Woodman.Common
     /// <summary>
     /// Отвечает за смену прозрачности у деревьев и домиков
     /// </summary>
-    [RequireComponent(typeof(MeshRenderer))]
     public class MeshTransparent : MonoBehaviour
     {
+        [SerializeField]
+        private MeshRenderer _meshRenderer;
+
         private bool _isChanged;
         private Material _material;
         private static readonly int Alpha = Shader.PropertyToID("_Alpha");
 
         private void Awake()
         {
-            _material = GetComponent<MeshRenderer>().material;
+            if (_meshRenderer == null)
+                _meshRenderer = GetComponent<MeshRenderer>();
+            _material = _meshRenderer.material;
         }
 
         public void SetTransparency(float transparency)
         {
-            // if (!_isChanged)
-            //     CopyMaterial();
             _material.SetFloat(Alpha, transparency);
         }
     }
