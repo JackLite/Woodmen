f0c1b4c92e212c8b12f19ad03604653c01e4ea2e
Sergey Dyuchkov <jackkdlite@gmail.com>
2022-11-29 02:14:55 +0600

    Run animation and fix joystick

--- Assets/Content/Codebase/Player/Movement/View/PlayerMovement.cs
+++ Assets/Content/Codebase/Player/Movement/View/PlayerMovement.cs
@@ -1,18 +1,21 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics;
+using UnityEngine;
+using Woodman.Utils;
 
 namespace Woodman.Player.Movement.View
 {
     public class PlayerMovement : MonoBehaviour
     {
-        private static readonly int StopWalk = Animator.StringToHash("StopWalk");
-        private static readonly int StartWalk = Animator.StringToHash("StartWalk");
-
         [SerializeField]
         private float _speed;
 
         [SerializeField]
         [Range(0, 1)]
         private float _inputThreshold = .4f;
+
+        [SerializeField]
+        [Range(0, 1)]
+        private float _sensitivity = 0.2f;
 
         [SerializeField]
         private Rigidbody _rigidbody;
@@ -23,52 +26,38 @@ namespace Woodman.Player.Movement.View
         [SerializeField]
         private Animator _animator;
 
-        [SerializeField]
-        private float _animationSpeedFactor = 1;
-
         private Vector2 _lastDelta;
-        public bool IsMoving => _rigidbody.velocity != Vector3.zero;
+        private static readonly int Speed = Animator.StringToHash("Speed");
         public Vector3 CurrentPos => transform.position;
 
         public void Move(Vector2 delta)
         {
-            var mg = delta.magnitude;
-            if(mg < _inputThreshold)
-                delta = Vector2.zero;
-            var direction = new Vector3(delta.x, 0, delta.y);
-            _rigidbody.AddForce(direction * _speed, ForceMode.Acceleration);
-            UpdateAnimation(delta);
-            if (delta == Vector2.zero)
+            var reverseSens = 1 - _sensitivity;
+            var absX = math.abs(delta.x);
+            var absY = math.abs(delta.y);
+            if (absX > reverseSens || absY > reverseSens)
             {
-                _animator.speed = 1;
+                delta = delta.normalized;
+            }
+            else
+            {
+                var x = 1 - (reverseSens - delta.x) / reverseSens;
+                var y = 1 - (reverseSens - delta.y) / reverseSens;
+                delta = new Vector2(x, y);
+            }
+
+            var mg = delta.magnitude;
+            if (mg < _inputThreshold)
+            {
+                _animator.SetFloat(Speed, 0);
                 return;
             }
-            _animator.speed = delta.magnitude * _animationSpeedFactor;
+
+            var direction = new Vector3(delta.x, 0, delta.y);
+            _rigidbody.AddForce(direction * _speed, ForceMode.Acceleration);
+            _animator.SetFloat(Speed, mg);
 
             _character.rotation = Quaternion.LookRotation(new Vector3(delta.x, 0, delta.y));
-        }
-
-        public void StopMove()
-        {
-            _lastDelta = Vector2.zero;
-            _rigidbody.AddForce(Vector3.zero, ForceMode.Acceleration);
-            _animator.SetTrigger(StopWalk);
-        }
-
-        private void UpdateAnimation(Vector2 delta)
-        {
-            if (_lastDelta != Vector2.zero && delta == Vector2.zero)
-            {
-                _animator.ResetTrigger(StartWalk);
-                _animator.SetTrigger(StopWalk);
-            }
-            else if (_lastDelta == Vector2.zero && delta != Vector2.zero)
-            {
-                _animator.ResetTrigger(StopWalk);
-                _animator.SetTrigger(StartWalk);
-            }
-
-            _lastDelta = delta;
         }
     }
 }
